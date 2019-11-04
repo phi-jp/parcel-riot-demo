8d228430f130f07ef6ae1942a76e251de3eeb91f
phi <phi1618jp@gmail.com>
2019-11-05 02:31:17 +0900

    fix

--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import './tags/**/*.tag'
 // global に展開(他で import する必要がなくなる)
 window.Firestore = Firerest;
 
+// // package.json 読めるかの実験
+const config = require('../package.json');
+console.log(config);
+
 riot.mount('*');
 
 
